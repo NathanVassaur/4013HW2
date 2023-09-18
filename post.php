7dd8b59519ee16dd49eb773349575cd384b369e5
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-09-18 12:06:55 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Post";
+include "view-header.php";
+?>
+
+<h1>Post</h1>
+<form method="post" action-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data">
+
+<?php
+include "view-footer.php";
+?>
