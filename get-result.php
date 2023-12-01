70f5b485f2f52cf53339118a087efd1d099f7d95
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-11-30 23:01:30 -0600

    Update get-result.php

--- get-result.php
+++ get-result.php
@@ -1,1 +1,16 @@
+<?php
+$pageTitle = "Get Result";
+include "view-header.php";
+?>
+
+<h1>Get Result</h1>
+
+<?php
+if (isset($_GET['my-name'])) {
+    echo "<p>The value sent is: " . $_GET['my-name'] . "</p>";
+} else {
+    echo "<p>Nothing posted to the page.</p>";
+}
+include "view-footer.php";
+?>
 
