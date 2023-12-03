e5cae9d90f50cc4a5a10189c1593d820480a7eba
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 11:57:16 -0600

    Update post.php

--- post.php
+++ post.php
@@ -2,15 +2,12 @@
 $pageTitle = "Post";
 include "view-header.php";
 ?>
-
 <h1>Post</h1>
-
 <form method="post" action="post-result.php">
     <input type="text" name="my-name">
     <input type="submit" value="Post data">
 </form>
-
-<?php
+<?php>
 include "view-footer.php";
 ?>
 
