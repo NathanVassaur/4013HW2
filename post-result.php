6b7f209a6bcea3615202d81c88e9799a2853ae1c
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 11:42:46 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,14 +2,17 @@
 $pageTitle = "Post Result";
 include "view-header.php";
 ?>
-
 <h1>Post Result</h1>
-
 <?php
 if (isset($_POST['my-name'])) {
-    echo "<p>The value sent is: " . $_POST['my-name'] . "</p>";
+?>
+    <p>The value sent is:</p>
+<?php
+    echo $_POST['my-name'];
 } else {
-    echo "<p>Nothing posted to the page.</p>";
+?>
+    <p>Nothing posted to the page.</p>;
+<?php
 }
 include "view-footer.php";
 ?>
